deb88816968cc1d7f75db3d64639e2796570c104
Elisabete-MO <bete.mrt@gmail.com>
2026-01-19 11:55:42 -0300

    feat: adiciona estrutura inicial do projeto com função de contagem de palavras

---
+++ lista_1/exercicio_2/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function contarPalavras(texto) {
+    if (!texto.trim()) {
+        return 0;
+    }
+    return texto.trim().split(/\s+/).length;
+}
+console.log(contarPalavras("Olá mundo")); // 2
+console.log(contarPalavras("TypeScript é muito legal")); // 4
+console.log(contarPalavras("   muitos   espaços   ")); // 2
+console.log(contarPalavras("")); // 0
